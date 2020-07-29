fdc937bc1b5a96784909a4d320c7140b1844d92b
Stephan Chandler-Garcia <stephan@gdprsuperheroes.com>
2020-07-29 11:39:37 -0700

    update to apex classes

---
+++ README.md
@@ -0,0 +1,50 @@
+# LWC Scheduler Service
+
+Recently, I was building a Salesforce Labs App that required me to enable a user to schedule an Apex job from the UI. I quickly realized I didn't know enough about the process and found a lack of information about the topic. I've written a basic overview of how to manage scheduled jobs from the UI and give your apps a bit more setup capability.
+
+## Features
+
+The app demonstrates:
+
+- Array Destructuring
+- Template Literals
+- Scheduling Apex Jobs
+- Aborting Scheduled Jobs
+- Checking Schedule Job Status
+
+## Installing LWC Scheduler Service using a Scratch Org
+
+1. If you haven't already done so, authorize your hub org and provide it with an alias (**myhuborg** in the command below):
+
+   ```
+   sfdx force:auth:web:login -d -a myhuborg
+   ```
+
+1. Clone the repository:
+
+   ```
+   git clone https://github.com/schandlergarcia/communities-social-navigation
+   cd communities-social-navigation
+   ```
+
+1. Create a scratch org and provide it with an alias (**ebikes** in the command below):
+
+   ```
+   sfdx force:org:create -s -f config/project-scratch-def.json -a communities-navigation
+   ```
+
+1. Push the app to your scratch org:
+
+   ```
+   sfdx force:source:push
+   ```
+
+1. Open the scratch org:
+
+   ```
+   sfdx force:org:open
+   ```
+
+1. Create a remote site setting
+
+Navigate to Setup > Remote Site Settings > Add a remote site setting for your community url
---
+++ force-app/main/default/lwc/lwcScheduler/lwcScheduler.js
@@ -0,0 +1,151 @@
+import { LightningElement, track } from "lwc";
+import getCurrentlyScheduledCron from "@salesforce/apex/LWCSchedulingService.getCurrentlyScheduleCron";
+import runFirstJob from "@salesforce/apex/LWCSchedulingService.runFirstJob";
+import checkFirstJobStatus from "@salesforce/apex/LWCSchedulingService.checkFirstJobStatus";
+import scheduleJob from "@salesforce/apex/LWCSchedulingService.scheduleJob";
+import deleteScheduledJob from "@salesforce/apex/LWCSchedulingService.deleteScheduledJob";
+
+export default class LwcScheduler extends LightningElement {
+  cronJobName = "Create Daily Account Record";
+  methodName = "createAccountRecord";
+  @track currentCronAsTime;
+  currentCronAsString;
+  state; // test, schedule, reschedule
+  loading;
+
+  connectedCallback() {
+    this.loading = true;
+    this.getScheduledCron();
+  }
+
+  /**
+   * On component load - we want to check to see if the job is currently scheduled. If it is
+   * scheduled - we can modify the state appropriatley.
+   */
+
+  getScheduledCron() {
+    getCurrentlyScheduledCron({ cronJobName: this.cronJobName })
+      .then(result => {
+        switch (result) {
+          case "test":
+            this.state = result;
+            break;
+          case "schedule":
+            this.state = result;
+            break;
+          default:
+            this.currentCronAsTime = this.convertCronToTime(result);
+            console.log("Job Scheduled for: " + this.currentCronAsTime);
+            this.state = "reschedule";
+        }
+        this.stopLoading(500);
+      })
+      .catch(error => {
+        this.stopLoading(500);
+      });
+  }
+
+  convertCronToTime(result) {
+    let cronArray = result.split(" ");
+    let [second, minute, hour] = cronArray;
+    return `${hour}:${minute}:00.000`;
+  }
+
+  runFirstJob() {
+    this.loading = true;
+    runFirstJob({})
+      .then(data => {
+        this.checkFirstSecurityJobStatus();
+      })
+      .catch(error => {
+        this.stopLoading(500);
+      });
+  }
+
+  checkFirstJobStatus() {
+    checkFirstJobStatus({
+      submittedDatetime: this.dateTimeSubmitted,
+      methodName: this.methodName
+    })
+      .then(result => {
+        switch (result) {
+          case "Completed":
+            this.state = "schedule";
+            this.stopLoading;
+            break;
+          case ("Aborted", "Failed"):
+            this.stopLoading(500);
+            console.log(data.ExtendedStatus);
+          default:
+            setTimeout(() => {
+              console.log("Checking");
+              this.checkFirstJobStatus();
+            }, 100);
+        }
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  }
+
+  scheduleApexJob() {
+    this.loading = true;
+    scheduleJob({
+      cronString: this.currentCronAsString,
+      cronJobName: this.cronJobName
+    })
+      .then(data => {
+        console.log(data);
+
+        if (data) {
+          this.state = "reschedule";
+          this.getScheduledCron();
+        } else {
+          this.stopLoading(500);
+          console.log("Unable to Schedule Job");
+        }
+      })
+      .catch(error => {
+        this.stopLoading(500);
+        console.log(error.message);
+      });
+  }
+
+  deleteJob() {
+    this.loading = true;
+    deleteScheduledJob({ cronJobName: this.cronJobName })
+      .then(data => {
+        console.log(data);
+        if (data) {
+          this.state = "schedule";
+          this.currentCronAsTime = "";
+          this.stopLoading(500);
+          console.log("Job Deleted");
+        } else {
+          this.stopLoading(100);
+          console.log("we were unable to delete this job");
+        }
+      })
+      .catch(error => {
+        this.stopLoading(100);
+        console.log(error.message);
+      });
+  }
+
+  handleTimeChange(event) {
+    let time = event.target.value;
+    let [hour, minute, seconds] = time.split(":");
+    this.currentCronAsString = `0 ${minute} ${hour} ? * * *`;
+  }
+  /**
+   * The stopLoading utility is used to control a consistant state experience for the user - it ensures that
+   * we don't have a flickering spinner effect when the state is in flux.
+   * @param {timeoutValue} timeoutValue
+   */
+
+  stopLoading(timeoutValue) {
+    setTimeout(() => {
+      this.loading = false;
+    }, timeoutValue);
+  }
+}
